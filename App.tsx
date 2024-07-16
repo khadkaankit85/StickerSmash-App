a14e44c75e378bec96c80bc4e453d4b6b8feb542
Angkit Khadka <khadkaankit85@gmail.com>
2024-07-15 21:37:28 -0400

    Getting deeper into react native

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{
+          backgroundColor: "#fff",
+        }}
+      >
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
